229647e907ff04ff712bf0bd3734e50475cb09fb
unknown <msjeon9983@gmail.com>
2026-03-13 15:23:56 +0900

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,18 @@
+public class Hello {
+
+    /*
+    * public static void main(string[] args) 함수는 자바에서 가장 먼저실행되는 함수
+    *
+    * 즉, 자바 프로그램의 시작이 되는 함수이며, 반드시 Main 함수가 있어야지 자바가 실행가능함
+    * Main 함수가 없으면 실행이 안됨
+     */
+    public static void main(String[] atgs){
+
+        System.out.println("Hello! My First Java coding");
+        System.out.println("My First Java coding");
+        System.out.println("I am freshman attending Polytaek University");
+        System.out.println("Nice to meet you");
+        System.out.println("Have a nice day!");
+
+    }
+}
